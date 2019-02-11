a8ed072a0be3ffa43621a0ab76728d29a92ddb56
Marc Grimme <grimme@fidor.com>
2019-02-11 16:28:14 +0100

    Integrated simplecov-small-badge gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,31 +4,28 @@ require 'simplecov'
 require 'byebug'
 
 SimpleCov.start do
-  # module SimpleCovSmallBadge
-  #   class Formatter
-  #   end
-  # end
-  #
-  # SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
-  #   [
-  #     SimpleCov::Formatter::HTMLFormatter,
-  #     SimpleCovSmallBadge::Formatter
-  #   ]
-  # )
+  module SimpleCovSmallBadge
+    class Formatter
+    end
+  end
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::HTMLFormatter,
+      SimpleCovSmallBadge::Formatter
+    ]
+  )
 end
 
 SimpleCov.minimum_coverage 100
 
 # require 'simplecov-small-badge'
 require 'repo_small_badge'
+require 'simplecov_small_badge'
 require 'rubygems'
 require 'bundler/setup'
 
 Dir[File.join('./spec/support/*.rb')].each { |f| require f }
-
-# SimpleCovSmallBadge.configure do |config|
-#   # config.rounded_border = false
-# end
 
 RSpec.configure do |config|
   # some (optional) config here
